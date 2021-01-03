19e3fffb4d0ab115ecd3770531b78c0ad9a431bf
Jessica D <deck.jessica@gmail.com>
2021-01-03 10:37:48 -0500

    finished up generate readme file, no preview showing yet

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contribution](#contribution)
+  *[Tests](#tests)
+  *[Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  For any questions, you can visit the [GitHub page](https://github.com/${data.username}/${data.title}).
+  Or email directly to [email](mailto:${data.email}).
+
 
 `;
 }
